d3be9f72135a07efa670939356feb2840e15bcff
Kris Jordan <krisjordan@gmail.com>
2020-02-25 00:20:13 -0500

    Add lec11 examples

---
+++ src/11-classes-and-objects/00-pizza-price-app.ts
@@ -0,0 +1,30 @@
+import { print } from "introcs";
+
+import { Pizza } from "./Pizza";
+
+export let main = async () => {
+
+    // 1. Initialize a variable that holds a Pizza object and print it
+
+    // 2. Assign different values to each of its properties
+
+    // 3. Compute and print its price with the imported price function
+
+};
+
+let price = (pizza: Pizza): number => {
+    let cost = 0;
+
+    // Base Price
+    // - small: $7
+    // - medium: $9
+    // - large: $11
+
+    // Extra cheese adds $1
+
+    // Each topping adds $0.75
+
+    return cost;
+};
+
+main();
