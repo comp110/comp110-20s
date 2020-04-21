9e0c13cac92686e78615b9c1ad5e361240e9851d
Kris Jordan <krisjordan@gmail.com>
2020-04-21 08:12:13 -0400

    add 21

---
+++ src/21-array-methods/00-array-fmr-app.ts
@@ -0,0 +1,19 @@
+import { print } from "introcs";
+
+export let main = async () => {
+
+    let numbers = [-1, 0, 1, 2, 3];
+    let strings = ["one", "two", "three"];
+
+    let filtered = numbers;
+    // print(filtered);
+
+    let mapped = strings;
+    // print(strings);
+
+    let reduced = numbers;
+    // print(reduced);
+
+};
+
+main();
