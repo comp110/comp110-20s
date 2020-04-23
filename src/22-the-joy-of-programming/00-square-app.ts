810702db09c45fa4e7e59ab55fd958eb3950b8c7
Kris Jordan <krisjordan@gmail.com>
2020-04-23 09:09:36 -0400

    Add lec22

---
+++ src/22-the-joy-of-programming/00-square-app.ts
@@ -0,0 +1,10 @@
+import { forward, left, setSpeed } from "introcs/turtle";
+setSpeed(0.1);
+
+export let main = async () => {
+
+    // TODO: Draw a Square
+
+};
+
+main();
---
+++ src/22-the-joy-of-programming/02-happy-trees-app.ts
@@ -0,0 +1,9 @@
+import { forward, left, right, setSpeed, moveTo, setOnClick } from "introcs/turtle";
+
+export let main = async () => {
+
+    // TODO
+
+};
+
+main();
